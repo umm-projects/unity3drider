6206533d3614665d21473d5e846c47196b2f38cb
Tetsuya Mori <monry84@gmail.com>
2017-07-21 11:57:29 +0900

    :seedling: Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# What?
+
+* IntelliJ Rider の Unity Plugin をインストールします
+
+# Why?
+
+* 毎回開き直すのが面倒だったから作りました。
+
+# Install
+
+```shell
+$ npm install --no-save @umm/unity3drider
+```
+
+* `--no-save` オプションを付けないと、 Rider 使っていない環境にも配信されてしまうので注意が必要です
+
+# System Requirement
+
+* Unity 2017.1.0f3
+* IntelliJ Rider RC
+    * Unity Plugin 2.0.0
+
+# License
+
+Copyright (c) 2017 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
---
+++ scripts/postinstall.js
@@ -0,0 +1,50 @@
+var mkdirp = require('mkdirp');
+var path = require('path');
+var package = require('../package.json');
+var rsync = new (require('rsync'))();
+
+var script_directory = __dirname;
+// パッケージ名が @ で始まるならスコープ有りと見なす
+var has_scope = /^@/.test(package.name);
+
+if ('node_modules' != path.basename(path.resolve(script_directory, (has_scope ? '../' : '') + '../../'))) {
+  // 開発インストールの場合無視する
+  return;
+}
+
+// スクリプトの存在するディレクトリから見たパス
+var source = path.resolve(script_directory, '../Assets');
+var destination = path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules');
+// パッケージ名を PascalCase にして付与
+//   (ネームスペースを持つ場合、そのまま namespace + @ をプレフィックスにする)
+if (/^@/.test(package.name)) {
+  destination += '/' + package.name.replace(
+    /^@([^\/]+)\/(.*)$/,
+    function(match, namespace, package_name) {
+      return namespace + '@' + package_name;
+    }
+  );
+} else {
+  destination += '/' + package.name;
+}
+
+// 宛先ディレクトリを作る (mkdir -p)
+mkdirp(destination, function(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  // rsync を用いてファイルを再帰的にコピーする
+  rsync
+    .flags('az')
+    .delete()
+    .source(source)
+    .destination(destination)
+    .execute(function(err, code, command) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+});
